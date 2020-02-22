a4bc078466701a744eb82adc08c5fcc291a4c678
egeakat <egeakat@hotmail.com>
2020-02-22 21:06:30 +0300

    we can also remove

--- script.js
+++ script.js
@@ -23,15 +23,36 @@ form.onsubmit = function(){
 
 
 function addClass(name, hours){
+
+    let deleteList = document.getElementById("delete-list");
+    let deleteNode = document.createElement("li");
+    
+    deleteList.appendChild(deleteNode);
+    deleteNode.innerHTML = name;
+
+    deleteNode.onclick =  function(){
+        removeClass(deleteList, deleteNode, hours);
+    };
+    
+    
+
     hours.forEach(element => {
         let col = parseInt(element/10);
-        let row = element%10 +1
+        let row = element%10 +1;
         tableBody.children[row].children[col].innerHTML = name;
     });
+   // console.log(deleteNode.dataset.hours.split(",").map(Number));
 };
 
 
 
-function removeClass(nodeToRemove, dayId, hours){};
+function removeClass(deleteList, nodeToRemove, hours){
+    deleteList.removeChild(nodeToRemove);
+    hours.forEach(element => {
+        let col = parseInt(element/10);
+        let row = element%10 +1;
+        tableBody.children[row].children[col].innerHTML = "";
+    });
+};
 
 
